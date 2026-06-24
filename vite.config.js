0ed982f5b9f5155094376827b2add26d9e85d686
koh-you <koh-you@users.noreply.github.com>
2026-06-24 14:56:45 +0900

    Split link preview titles

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        attendance: "attendance.html"
+      }
+    }
+  },
   server: {
     port: 5173
   }
